59fcb9b5944dcccfb499f6d7f3fd58408e97731b
dah00 <61763829+dah00@users.noreply.github.com>
2021-01-19 19:47:28 -0800

    optimize my own code with a better solution

--- LongestSubWithoutRepeatingChara/src/sec1/Main.java
+++ LongestSubWithoutRepeatingChara/src/sec1/Main.java
@@ -3,7 +3,7 @@ package sec1;
 public class Main {
 
     public static void main(String[] args) {
-	    String str = "au";
+	    String str = "abcabcbb";
 	    Solution sol = new Solution();
         System.out.println(sol.lengthOfLongestSubstring(str));
     }
