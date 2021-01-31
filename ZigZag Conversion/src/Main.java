57555ee4a618108416c82f5d969ee38e17235c4d
dah00 <61763829+dah00@users.noreply.github.com>
2021-01-31 15:07:24 -0800

    ZigZag conversion first try (unfinished)

---
+++ ZigZag Conversion/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "obeda";
+        for (char chr : str.toCharArray()){
+            System.out.println(chr);
+        }
+
+    }
+}
---
+++ ZigZag Conversion/src/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public String convert(String s, int numRows){
+        for (int i=0; i<s.length(); i++){
+
+        }
+
+        return "s";
+    }
+}
