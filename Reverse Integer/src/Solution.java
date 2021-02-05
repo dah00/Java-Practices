658b0aeb6ced6bad3c524e69c63b4761d10770c6
dah00 <61763829+dah00@users.noreply.github.com>
2021-02-05 13:12:50 -0800

    Best solution

--- Reverse Integer/src/Solution.java
+++ Reverse Integer/src/Solution.java
@@ -1,18 +1,36 @@
 public class Solution {
     public int reverse(int x){
+        int reversed = 0;
+
+        while(x!=0){
+            int pop = x%10;
+            x = x/10;
+            if (reversed > Integer.MAX_VALUE/10 || (reversed == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (reversed < Integer.MIN_VALUE/10 || (reversed == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            reversed = reversed * 10 + pop;
+        }
+        return reversed;
+
+
+        /**
+         * My own solution
+         * Failed to check if reversed is overflow
+         */
+        /*
         int newX = Math.abs(x);
         int numDigits = String.valueOf(newX).length();
-        int res = 0;
+        int reversed = 0;
         for (int i=0; i<numDigits; i++){
             if (newX/10 != 0){
-                int rest = newX%10;
-                res += rest;
-                res *= 10;
+                int pop = newX%10;
+                reversed = reversed * 10 + pop;
+                if (reversed>Integer.MAX_VALUE)
+                    return 0;
                 newX = newX/10;
             }
             else
-                res += newX;
+                reversed = reversed * 10 + newX;
         }
-        return (x<0) ? (res * -1) : res;
+        return (x<0) ? (reversed * -1) : reversed;*/
     }
 }
