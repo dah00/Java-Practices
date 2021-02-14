c4cfb4f7dac12fc27c035cf00f592b82f49c2a96
dah00 <61763829+dah00@users.noreply.github.com>
2021-02-14 13:33:37 -0800

    String to Integer first commit
    first tempt of trying to converty a string into an integer

---
+++ String to Integer/src/Solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int myAtoi(String s){
+        int result;
+        StringBuilder strDigit = new StringBuilder();
+
+        //1. Read in and ignore any leading whitespace.
+        s.trim();
+
+        for(int index=0; index<s.length(); index++){
+            char chr = s.charAt(index);
+
+            //2. Check if next Char (if not the end) is  '-' or '+', read the char in
+            boolean isNegative = false;
+            if (chr == '-')
+                isNegative = true;
+
+            //3. Read in next char until next is either non-digit or reach the end
+            if (Character.isDigit(chr))
+                strDigit.append(chr);               // May not work, use Integer.parseInt(String.valueOf(chr)
+
+        }
+        //4. convert into integer
+        result = Integer.parseInt(strDigit.toString());
+
+        //5. if result is out of 32-bit signed integer range, clamp it.
+        return result;
+    }
+}
