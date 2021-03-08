dd6b573a672b955abf57b06770a784abb638ecb8
dah00 <61763829+dah00@users.noreply.github.com>
2021-03-08 10:28:53 -0800

    2nd try of Merge Two Sorted Lists using recursion
    shorten the length of the code with the same time complexity and the memory complexity by using recursion

--- Merge Two Sorted Lists/src/Main.java
+++ Merge Two Sorted Lists/src/Main.java
@@ -1,10 +1,50 @@
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        l1.next.next.next = null;
+
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        l2.next.next.next = null;
+
+        ListNode result = mergeTwoLists(l1, l2);
+
+        while(result != null){
+            System.out.println(result.val);
+            result = result.next;
+        }
 
     }
 
-    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /*
+        1st try: Brute force solution
+        0ms, 38.5 MB
+        O(n)
+     */
+    /*private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null && l2 == null)
             return null;
 
@@ -81,21 +121,25 @@ public class Main {
 
 
         return head;
-    }
+    }*/
 
-    private class ListNode {
-        int val;
-        ListNode next;
 
-        ListNode() {}
 
-        ListNode(int val) {
-            this.val = val;
-        }
 
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
+    /*
+        2nd try: Using recusion
+        1ms,
+
+     */
+    private static ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
     }
 }
