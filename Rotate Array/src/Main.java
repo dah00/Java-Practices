9bb8cdd3126c3df103505afbd430960d865d6fe0
dah00 <61763829+dah00@users.noreply.github.com>
2021-03-26 10:44:38 -0700

    Rotate Array (LeetCode)
    Rotating/Moving all elements of an array k-times to the right in-place

--- Rotate Array/src/Main.java
+++ Rotate Array/src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        rotate(nums, 3);
+	    System.out.println(Arrays.toString(nums));
+
+    }
+
+    private static void rotate(int[] nums, int k){
+        if(nums.length>1){
+            int i = 1;
+            while(i <= k){
+                int temp = nums[0];
+                nums[0] = nums[nums.length-1];
+                for(int j=nums.length-2; j>=1; j--)
+                    nums[j+1] = nums[j];
+                nums[1] = temp;
+                i++;
+            }
+        }
     }
 }
