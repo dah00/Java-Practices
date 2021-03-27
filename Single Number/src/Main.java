ff08d32f2e67fc3c2a9f2f057288570e22d86449
dah00 <61763829+dah00@users.noreply.github.com>
2021-03-27 10:44:19 -0700

    update -single number
    better algorithm using bitwise XOR

--- Single Number/src/Main.java
+++ Single Number/src/Main.java
@@ -4,11 +4,19 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(3^5);
     }
 
     private int singleNumber(int[] nums) {
-        Set<Integer> set = new HashSet<>((nums.length/2)+1);
+        // O(n) with 1ms using XOR
+        int a = 0;
+        for (int i : nums) {
+            a ^= i;
+        }
+        return a;
+
+        // O(n) with 8ms
+        /*       Set<Integer> set = new HashSet<>((nums.length/2)+1);
         for(int element : nums){
             if (set.contains(element))
                 set.remove(element);
@@ -16,6 +24,6 @@ public class Main {
                 set.add(element);
         }
         Integer value = set.toArray(new Integer[1])[0];
-        return value;
+        return value;*/
     }
 }
