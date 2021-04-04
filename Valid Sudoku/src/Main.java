0cf85e2e7493c9f84017e008ac8aad7f77c07686
dah00 <61763829+dah00@users.noreply.github.com>
2021-04-04 11:40:43 -0700

    Update solution of Valid Sudoku
    Using a Set to store elements while checking for duplicate
    !set.add(number + "in row " + i) ||
                            !set.add(number + "in column " + j) ||
                            !set.add(number + "in box " + i/3 + "-" + j/3))

--- Valid Sudoku/src/Main.java
+++ Valid Sudoku/src/Main.java
@@ -4,8 +4,22 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Set seen = new HashSet();
+	    
     }
+
+    /*
+    [[".",".",".",".","5",".",".","1","."],
+     [".","4",".","3",".",".",".",".","."],
+     [".",".",".",".",".","3",".",".","1"],
+     ["8",".",".",".",".",".",".","2","."],
+     [".",".","2",".","7",".",".",".","."],
+     [".","1","5",".",".",".",".",".","."],
+     [".",".",".",".",".","2",".",".","."],
+     [".","2",".","9",".",".",".",".","."],
+     [".",".","4",".",".",".",".",".","."]]
+
+     */
 
     /*
         put each row then column in a Set while checking
@@ -13,6 +27,23 @@ public class Main {
      */
     private static boolean isValidSudoku(char[][] board){
 
+        //
+        /*Set set = new HashSet();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char number = board[i][j];
+                if (number != '.')
+                    if (!set.add(number + "in row " + i) ||
+                        !set.add(number + "in column " + j) ||
+                        !set.add(number + "in box " + i/3 + "-" + j/3))
+                        return false;
+            }
+        }
+
+        return true;*/
+
+
+        /*
         // Checking each row
         for (int i=0; i<9; i++){
             for (int j=0; j<9; j++){
@@ -37,6 +68,38 @@ public class Main {
             }
         }
 
-        return true;
+        // Checking each 3x3 sub-boxes
+        int i = 0;
+        while(i<9){
+            for (int j=0; j<3; j++){
+                Set<Character> set = new HashSet<>();
+                if (board[i][j] != '.'){
+                    if (set.contains(board[i][j]))
+                        return false;
+                    set.add(board[i][j]);
+                }
+            }
+
+            for (int j=3; j<6; j++){
+                Set<Character> set = new HashSet<>();
+                if (board[i][j] != '.'){
+                    if (set.contains(board[i][j]))
+                        return false;
+                    set.add(board[i][j]);
+                }
+            }
+
+            for (int j=6; j<9; j++){
+                Set<Character> set = new HashSet<>();
+                if (board[i][j] != '.'){
+                    if (set.contains(board[i][j]))
+                        return false;
+                    set.add(board[i][j]);
+                }
+            }
+            i+=3;
+        }
+
+        return true;*/
     }
 }
