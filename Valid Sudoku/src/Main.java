b0491e291288f0560a0b8f3d42948950ccd97ceb
dah00 <61763829+dah00@users.noreply.github.com>
2021-04-04 12:11:31 -0700

    Best solution so far for Valid Sudoku
    Store in a array of HashMap<int, int> each rows, columns, and boxes: Key = element of the sudoku, Value = number of repetition.
    Then check if value > 1 return false

--- Valid Sudoku/src/Main.java
+++ Valid Sudoku/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -21,11 +22,45 @@ public class Main {
 
      */
 
-    /*
-        put each row then column in a Set while checking
-        if the Set contains the element already
+    /**
+       HashMap method: rows[i].getOrDefault(n, 0)
+        => This method returns value mapped with the specified key,
+            otherwise default value is returned.
+
      */
     private static boolean isValidSudoku(char[][] board){
+
+        HashMap<Integer, Integer>[] rows = new HashMap[9];
+        HashMap<Integer, Integer>[] columns = new HashMap[9];
+        HashMap<Integer, Integer>[] boxes = new HashMap[9];
+        for (int i=0; i<9; i++){
+            rows[i] = new HashMap<Integer, Integer>();
+            columns[i] = new HashMap<Integer, Integer>();
+            boxes[i] = new HashMap<Integer, Integer>();
+        }
+
+        for (int i=0; i<9; i++){
+            for (int j=0; j<9; j++){
+                char num = board[i][j];
+                if (num != '.'){
+                    int n = (int)num;
+                    int box_index = (i/3)*3 + j/3;
+
+                    // keep the current cell value
+                    rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);
+                    columns[j].put(n, columns[j].getOrDefault(n, 0) + 1);
+                    boxes[box_index].put(n, boxes[box_index].getOrDefault(n, 0) + 1);
+
+                    // check if this value has been already seen before
+                    if (rows[i].get(n)>1 || columns[j].get(n)>1 || boxes[box_index].get(n)>1)
+                        return false;
+                }
+            }
+        }
+
+        return true;
+
+
 
         //
         /*Set set = new HashSet();
