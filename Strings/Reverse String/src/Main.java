e179081d6dad76f3f343a2f8abefa0d1b2308d01
dah00 <61763829+dah00@users.noreply.github.com>
2021-04-06 10:49:49 -0700

    LeetCode practice  - Reverse String
    have an index at the beginning(i) and the end(j) of the array
            swap s[i] wiht s[j]
            i++ and j--
            until i>j

--- Strings/Reverse String/src/Main.java
+++ Strings/Reverse String/src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    }
+    /*
+        Write a function that reverses a string.
+        The input string is given as an array of characters s.
+        modifying the input array in-place with O(1)
+     */
+    private static void reverseString(char[] s) {
+        // have an index at the beginning(i) and the end(j) of the array
+        // swap s[i] wiht s[j]
+        // i++ and j--
+        // until i>j
+        int i=0, j=s.length-1;
+        while (i<j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++; j--;
+        }
+
     }
 }
