0b0c0a33d891a78f8801293684e6f2d6c2ead2ad
dah00 <61763829+dah00@users.noreply.github.com>
2021-04-12 09:54:47 -0700

    Update Valid Palindrome
    1- convert all the character of the string to lower case
    2- remove all non-alphanumeric character off the string
    3- check if the character at the beginning of the string is the same as the character at the end, if not return false

--- Strings/Valid Palindrome/src/Main.java
+++ Strings/Valid Palindrome/src/Main.java
@@ -4,26 +4,30 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-        
+        String str = "0P";
+        System.out.println(isPalindrome(str));
     }
 
-    private boolean isPalindrome(String s){
-
-        Set<Character> set = new HashSet<>();
+    private static boolean isPalindrome(String s){
         s = s.toLowerCase();
+        StringBuffer sAlphNum = new StringBuffer();
 
         for (int i=0; i<s.length(); i++){
             char chr = s.charAt(i);
-            if (chr>='a' && chr<='z' || chr >= 'A' && chr <= 'Z'){
-                if (!set.contains(chr))
-                    set.add(chr);
-                else
-                    set.remove(chr);
-            }
+            if (chr>='a' && chr<='z' ||
+                chr>='A' && chr<='Z' ||
+                chr>='0' && chr<='9')
+                sAlphNum.append(chr);
         }
 
-        if (set.size() <2)
-            return true;
-        return false;
+        s = sAlphNum.toString();
+        int i=0, j=s.length()-1;
+
+        while(i<=j){
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++; j--;
+        }
+        return true;
     }
 }
