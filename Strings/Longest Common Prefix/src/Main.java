c9811625f2aca0fee19d98b26c654d531bdade4a
dah00 <61763829+dah00@users.noreply.github.com>
2021-04-22 12:29:02 -0700

    Update Longest Common Prefix
    code successful run with runtime 1ms

--- Strings/Longest Common Prefix/src/Main.java
+++ Strings/Longest Common Prefix/src/Main.java
@@ -1,45 +1,34 @@
 public class Main {
 
     public static void main(String[] args) {
-        String str = "obeda";
-        String o = "oe";
-        System.out.println(str.contains(o));
+        String[] str = {"ab", "a"};
+
+        System.out.println(longestCommonPrefix(str));
     }
 
     private static String longestCommonPrefix(String[] strs){
         // Brute force
-        if (strs.length == 0)
-            return "";
 
+        StringBuilder stringBuilder = new StringBuilder();
 
-        // for all substring of the first element of strs possible
-        // if the rest of the element of the strs contains that substring
-            // store it in a variable result
-            // append the next character of the first element and check
-        String result = "";
-        int initialIndexSub = 0;
+        for(int i=0; i<strs[0].length(); i++){
+            char chr = strs[0].charAt(i);
 
-        for (int i=0; i<strs[0].length()-1; i++){
-            StringBuilder stringBuilder = new StringBuilder(strs[0].substring(initialIndexSub, i+1));
             boolean contains = true;
-
-
-            int index = 1;              // traversing strs starting from the second element
-            while (index < strs.length && contains){
-                String str = strs[index++];         // to check if index++ is valid
-                if (!str.contains(stringBuilder.toString()))
+            int index = 1;
+            while(index<strs.length && contains){
+                if (i>=strs[index].length() || !(strs[index].charAt(i) == chr))
                     contains = false;
+                index++;
             }
 
-            if (contains && result.length() < stringBuilder.length() ){
-                result = stringBuilder.toString();
-            }
+            if (contains)
+                stringBuilder.append(chr);
             else
-                initialIndexSub = i;
-
+                break;
         }
 
-        return result;
+        return stringBuilder.toString();
 
     }
 }
