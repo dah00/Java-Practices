5a752e1987e352ad41aa8b5ef1c1ef0c6a132679
dah00 <61763829+dah00@users.noreply.github.com>
2021-07-06 17:42:34 -0700

    LeetCode practice - count Binary Substring

---
+++ Strings/Count Binary Substings/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    static int countBinarySubstrings(String s){
+        int[] groups = new int[s.length()];
+        int t = 0;
+        groups[0] = 1;
+        for (int i=1; i< s.length(); i++){
+            if (s.charAt(i-1) != s.charAt(i)){
+                groups[++t] = 1;
+            }else{
+                groups[t]++;
+            }
+        }
+
+        int ans = 0;
+        for(int i=0; i<=t; i++){
+            ans += Math.min(groups[i-1], groups[i]);
+        }
+
+        return ans;
+    }
+
+}
