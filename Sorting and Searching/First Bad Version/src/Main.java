096ba3f545a72f022775fabf73948eebf5eb073a
dah00 <61763829+dah00@users.noreply.github.com>
2021-08-16 09:39:14 -0700

    LeetCode Binary Search -"First Bad Version"

--- Sorting and Searching/First Bad Version/src/Main.java
+++ Sorting and Searching/First Bad Version/src/Main.java
@@ -3,4 +3,19 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    static int firstBadVersion(int n){
+        int left = 1, right = n;
+
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(isBadVersion(mid)){
+                if(mid==1 || !isBadVersion(mid-1))
+                    return mid;
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+    }
 }
